0cf5d253b0b48a02d52502ba8ecb342466e046c4
Nils Lockean <nilslockean@gmail.com>
2026-03-06 13:32:55 +0100

    Add page to schemaTypes

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,5 +4,6 @@ import orderTerms from './orderTerms'
 import product from './product'
 import order from './order'
 import orderItem from './orderItem'
+import page from './page'
 
-export const schemaTypes = [openingHours, faq, orderTerms, product, order, orderItem]
+export const schemaTypes = [openingHours, faq, orderTerms, product, order, orderItem, page]
